daeac6febfe0ba6e3d89b45524a452cbf9201254
Gleb072007 <figmintov@mail.ru>
2021-11-10 22:50:46 +0300

    First commit

---
+++ app/src/main/java/ru/iot3/my_testing_fragments_application/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.iot3.my_testing_fragments_application
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
